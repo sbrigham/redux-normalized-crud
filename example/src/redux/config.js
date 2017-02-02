374192e0bdb211db3ea5c259073cf1c65353f303
Spencer Brigham <sbrigham@campuslabs.com>
2017-02-02 11:49:11 -0500

    bring in fetch interceptor to show an example on how to set custom headers before the request is sent

--- example/src/redux/config.js
+++ example/src/redux/config.js
@@ -1,4 +1,25 @@
 import { normalize } from 'normalizr';
+import fetchIntercept from 'fetch-intercept';
+
+fetchIntercept.register({
+    request: function (url, config) {
+      // update headers here
+      return [url, config];
+    },
+
+    requestError: function (error) {
+      // Called when an error occured during another 'request' interceptor call
+      return Promise.reject(error);
+    },
+
+    response: function (response) {
+      return response;
+    },
+
+    responseError: function (error) {
+      return Promise.reject(error);
+    }
+});
 
 export default {
   baseUrl: 'https://jsonplaceholder.typicode.com/',
